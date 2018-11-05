d71e400bfa1d716927ca0df5fe39ad331724b8b6
Sinan Emiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2018-11-06 00:30:37 +0200

    Menu+CameraZoom
    I will make it more elegant later.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float zoomSpeed = .1f;
+    public Transform targetPlanet;
+
+    private float cameraSize;
+    private bool isStarted = false;
+
+    void Start () {
+        cameraSize = GetComponent<Camera>().orthographicSize;
+	}
+	
+    private void LateUpdate()
+    {
+        if (isStarted)
+        {
+            ZoomIntoPlanet();
+        }
+    }
+    private void ZoomIntoPlanet()
+    {
+        transform.LookAt(targetPlanet);
+        if (cameraSize > 0.15f)
+        {
+            cameraSize -= zoomSpeed * Time.deltaTime;
+        }
+        GetComponent<Camera>().orthographicSize = cameraSize;
+    }
+    public void GameStarted()
+    {
+        isStarted = true;
+    }
+}
