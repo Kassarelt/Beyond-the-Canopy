62450bd62074516e91b8d2d6b9fdcfc2861a3cb0
RalphSG <43961722+RalphSG@users.noreply.github.com>
2018-11-09 23:31:54 +0200

    Main menu scene with transition to next scene + small reorganisation of the folders

---
+++ Assets/Scripts/GameVersion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameVersion : MonoBehaviour {
+
+	public Text gameVersionText;
+
+	// Use this for initialization
+	void Start () {
+		gameVersionText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameVersionText.text = "Gametitle Whatever version alpha " + Application.version;
+	}
+}
---
+++ Assets/Scripts/SceneTransitionMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionMenu : MonoBehaviour {
+
+	public Animator transitionAnim;
+	public string sceneName1;
+	public string sceneName2;
+	public string sceneName3;
+	public string sceneName4;
+
+	public void NewGame(){
+		StartCoroutine (LoadScene1());
+	}
+
+	IEnumerator LoadScene1(){
+		transitionAnim.SetTrigger ("end");
+		yield return new WaitForSeconds (2.5f);
+		SceneManager.LoadScene (sceneName1);
+	}
+
+	public void LoadGame(){
+		StartCoroutine (LoadScene2());
+	}
+
+	IEnumerator LoadScene2(){
+		transitionAnim.SetTrigger ("end");
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (sceneName2);
+	}
+
+	public void Options(){
+		StartCoroutine (LoadScene3());
+	}
+
+	IEnumerator LoadScene3(){
+		transitionAnim.SetTrigger ("end");
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (sceneName3);
+	}
+
+	public void Quit(){
+		StartCoroutine (LoadScene4());
+	}
+
+	IEnumerator LoadScene4(){
+		transitionAnim.SetTrigger ("end");
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (sceneName4);
+	}
+}
