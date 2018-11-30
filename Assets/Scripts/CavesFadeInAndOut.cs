fa36272ad5ae40d632f8714f9a583ddfe26b93e9
RalphSG <43961722+RalphSG@users.noreply.github.com>
2018-11-30 16:25:11 +0200

    FadeIn and Out for the first cave

---
+++ Assets/Scripts/CavesFadeInAndOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CavesFadeInAndOut : MonoBehaviour {
+
+    private Animator transitionAnim;
+
+	// Use this for initialization
+	void Start () {
+        transitionAnim = gameObject.GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transitionAnim.SetTrigger("enter");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transitionAnim.SetTrigger("enter");
+        }
+    }
+}
