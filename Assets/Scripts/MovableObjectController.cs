9c04016f14cd7ab077298add8a7add2e82b5fe87
Sinan Emiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2018-12-01 21:48:51 +0200

    FixedUpdate

---
+++ Assets/Scripts/MovableObjectController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObjectController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag == "Platform")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+        if (collision.gameObject.tag == "movablePlatform")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag == "Platform")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+        if (collision.gameObject.tag == "movablePlatform")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag == "Platform")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
