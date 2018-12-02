112fff74e48c6fd09a76e3509c05673d83ec69b9
RalphSG <43961722+RalphSG@users.noreply.github.com>
2018-12-02 02:28:39 +0200

    Added animation main menu

--- Assets/Scripts/SceneTransitionMenu.cs
+++ Assets/Scripts/SceneTransitionMenu.cs
@@ -62,4 +62,16 @@ public class SceneTransitionMenu : MonoBehaviour {
         yield return new WaitForSeconds(2.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void Quit()
+    {
+        StartCoroutine(QuitGame());
+    }
+
+    IEnumerator QuitGame()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(2.5f);
+        Application.Quit();
+    }
 }
