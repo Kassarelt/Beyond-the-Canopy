34e4f85b1ea4222f787bc4da838caa8023ccfbcd
RalphSG <43961722+RalphSG@users.noreply.github.com>
2018-12-06 04:44:17 +0200

    Added names and transition control for intro scene

---
+++ Assets/Scripts/IntroSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSceneManager : MonoBehaviour
+{
+
+    public Animator transitionAnim;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(3.45f);
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(2.3f);
+        SceneManager.LoadScene("Menu");
+    }
+}
